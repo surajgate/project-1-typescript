730176bb08ee9653569fd0d1ffb59d244d647794
suraj gate <surajgate73@gmail.com>
2023-05-09 12:45:37 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+// import List, { PeopleType } from "./List";
+import data from './data';
+import List from './List';
+
+
+
 
 function App() {
+
+   const [boolean, setBoolean] = useState<boolean>(true);
+   const [peoples, setPeoples] = useState<any>(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{peoples.length} birthdays today</h3>
+        {(boolean)?<List/>: ""}
+        <button onClick={ () => {setBoolean(false); setPeoples([]) } }>Clear All</button>
+      </section>
+    </main>
   );
+
 }
+
 
 export default App;
